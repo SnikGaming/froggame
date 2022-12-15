f8da54eaefaa419d44a8c9537ca2fa34a8db78ab
TranThoiLong <tranthoilong.giaitri@gmail.com>
2022-12-15 16:47:46 +0700

    hoan thanh man choi.

---
+++ froggame/lib/view_data/firesore_questions.dart
@@ -0,0 +1,38 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class FureStoreQuestions {
+  static final questions = FirebaseFirestore.instance.collection("questions");
+  static final categories = FirebaseFirestore.instance.collection("categoties");
+
+  static var lsData = [];
+  static var lsDataLeng = [];
+  static int _categories = 0;
+  static var count = [];
+
+  static getLenght() async {
+    await categories.get().then((value) {
+      for (var i in value.docs) {
+        lsDataLeng.add(i.data()["id"]);
+      }
+      _categories = lsDataLeng.length;
+    });
+    for (var i = 0; i < _categories; i++) {
+      await FirebaseFirestore.instance
+          .collection('questions')
+          .where("idlv", isEqualTo: i + 1)
+          .get()
+          .then((value) {
+        for (var a in value.docs) {
+          lsData.add(a.data()["id"]);
+        }
+      });
+      count.add(lsData.length);
+      lsData = [];
+    }
+
+    //!:
+    for (var i = 0; i < count.length; i++) {
+      print("===============QWWWWWWWWWWWWWW${count[i]}");
+    }
+  }
+}
